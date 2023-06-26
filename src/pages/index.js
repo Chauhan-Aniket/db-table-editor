61215699830ea2faf3c932b752031da3126ec5d8
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2023-06-26 11:03:55 +0530

    style: add mantine provider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import { MantineProvider, useMantineTheme } from "@mantine/core";
+
 import DataTable from "@/components/DataTable";
 import InputText from "@/components/InputText";
-
 import MOCK_DATA from "@/components/mock-data.json";
 
 const Home = () => {
+	const globalTheme = useMantineTheme();
+
 	const [host, setHost] = useState("");
 	const [userName, setUserName] = useState("");
 	const [password, setPassword] = useState("");
@@ -43,7 +46,6 @@ const Home = () => {
 		}
 	};
 
-	console.log(jsonData[0]);
 	const handleSaveCell = async (cell, value) => {
 		const newJsonData = jsonData.length > 0 ? [...jsonData] : [...MOCK_DATA];
 
@@ -87,64 +89,72 @@ const Home = () => {
 	};
 
 	return (
-		<main
-			className={`dark relative h-screen w-full flex bg-zinc-50 dark:bg-zinc-900`}
+		<MantineProvider
+			theme={{
+				...globalTheme,
+				primaryColor: "blue",
+				colorScheme: "dark",
+			}}
+			withGlobalStyles
+			withNormalizeCS
 		>
-			<form
-				onSubmit={fetchData}
-				className="w-fit p-4 flex flex-col justify-between text-zinc-900 bg-zinc-50 dark:text-zinc-50 dark:bg-zinc-900 border-r border-zinc-200 dark:border-zinc-800"
+			<main
+				className={`dark relative h-screen w-full flex bg-zinc-50 dark:bg-zinc-900`}
 			>
-				<div className="w-full flex flex-col gap-6">
-					<InputText
-						id="host"
-						value={host}
-						handleChange={(e) => setHost(e.target.value)}
-					>
-						Host
-						<span className="text-zinc-400 float-right">IP | URL</span>
-					</InputText>
-					<InputText
-						id={"username"}
-						value={userName}
-						handleChange={(e) => setUserName(e.target.value)}
-					>
-						Username
-					</InputText>
-					<InputText
-						id={"password"}
-						value={password}
-						handleChange={(e) => setPassword(e.target.value)}
-					>
-						Password
-					</InputText>
-					<hr className="border-zinc-200 dark:border-zinc-800" />
-					<InputText
-						id="database name"
-						value={databaseName}
-						handleChange={(e) => setDatabaseName(e.target.value)}
-					>
-						Database Name
-					</InputText>
-					<InputText
-						id="table name"
-						value={tableName}
-						handleChange={(e) => setTableName(e.target.value)}
-					>
-						Table Name
-					</InputText>
-				</div>
-				<button
-					type="submit"
-					className="p-2.5 px-6 text-white bg-blue-700 text-xs font-medium tracking-wide rounded"
+				<form
+					onSubmit={fetchData}
+					className="w-fit p-4 flex flex-col justify-between text-zinc-900 bg-zinc-50 dark:text-zinc-50 dark:bg-zinc-900 border-r border-zinc-200 dark:border-zinc-800"
 				>
-					Fetch Data
-				</button>
-			</form>
+					<div className="w-full flex flex-col gap-6">
+						<InputText
+							id="host"
+							value={host}
+							handleChange={(e) => setHost(e.target.value)}
+						>
+							Host
+							<span className="text-zinc-400 float-right">IP | URL</span>
+						</InputText>
+						<InputText
+							id={"username"}
+							value={userName}
+							handleChange={(e) => setUserName(e.target.value)}
+						>
+							Username
+						</InputText>
+						<InputText
+							id={"password"}
+							value={password}
+							handleChange={(e) => setPassword(e.target.value)}
+						>
+							Password
+						</InputText>
+						<hr className="border-zinc-200 dark:border-zinc-800" />
+						<InputText
+							id="database name"
+							value={databaseName}
+							handleChange={(e) => setDatabaseName(e.target.value)}
+						>
+							Database Name
+						</InputText>
+						<InputText
+							id="table name"
+							value={tableName}
+							handleChange={(e) => setTableName(e.target.value)}
+						>
+							Table Name
+						</InputText>
+					</div>
+					<button
+						type="submit"
+						className="p-2.5 px-6 text-white bg-blue-700 text-xs font-medium tracking-wide rounded"
+					>
+						Fetch Data
+					</button>
+				</form>
 
-			{/* <div className="p-4 h-screen w-full dark:bg-zinc-900 grow"> */}
-			<DataTable jsonData={jsonData} handleSaveCell={handleSaveCell} />
-			{/* </div> */}
-		</main>
+				<DataTable jsonData={jsonData} handleSaveCell={handleSaveCell} />
+			</main>
+		</MantineProvider>
 	);
 };
 
